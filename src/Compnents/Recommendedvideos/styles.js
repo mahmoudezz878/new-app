8e1098968692e8d12f45da96ed6121efff9bf595
mahmoudezz878 <mahmoudezz878@gmail.com>
2021-08-10 18:32:35 +0200

    r

---
+++ src/Compnents/Recommendedvideos/styles.js
@@ -0,0 +1,38 @@
+const styles = (theme) => ({
+  recommendedvideos: {
+    flex: "1",
+    backgroundColor: theme.palette.type === "dark" ? "#181818" : "#f9f9f9",
+    paddingLeft: "20px",
+    paddingBottom: "0",
+    position: "relative",
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft:'0',
+    },
+    "& h2": {
+      marginLeft: "5px",
+      marginBottom: "20px",
+    },
+  },
+  recommendedvideosVideos: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: 'flex-start',
+    [theme.breakpoints.down('xs')]: {
+      padding:'0',
+      flexDirection: 'column',
+    },
+  },
+  loading: {
+    margin: "auto",
+    display: "block !important",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+  },
+  recommendedvideosLink: {
+    textDecoration: "none",
+    color: theme.palette.type === "dark" ? "#FFF" : "#000",
+  },
+});
+
+export default styles;
---
+++ src/Compnents/Sidebar/styles.js
@@ -0,0 +1,23 @@
+const styles = (theme) => ({
+    sidebare: {
+        width: '220px',
+        [theme.breakpoints.down('xs')]: {
+            display: 'none',
+            backgroundColor: 'red',
+          },
+        backgroundColor: theme.palette.type === 'dark' ? '#212121' : '#FFF',
+        '& hr': {
+            height: '1px',
+            border: '0',
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        },
+        '& a': {
+            textDecoration: 'none',
+        }
+    },
+    hidesidebar: {
+        display: 'none',
+    }
+})
+
+export default styles
